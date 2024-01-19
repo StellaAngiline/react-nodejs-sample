7b40de20da086975fab13c144eae8307e8f2f1f4
stella <stellaangiline@gmail.com>
2024-01-19 11:46:00 +0800

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './App.css';
+
+class App extends React.Component{
+
+  constructor(props){
+
+    super(props)
+
+    this.state={apiResponse:""};
+
+  }
+  callAPI(){
+
+    fetch("http://localhost:9000/testAPI")
+
+    .then(res=>res.text())
+
+    .then(res=>this.setState({apiResponse:res}))
+
+  }
+
+  componentWillMount(){
+
+    this.callAPI()
+
+  }
+
+render() {
+
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+
+        <p>{this.state.apiResponse}</p>
+
+      </header>
+
+    </div>
+
+  );
+
+}
+
+}
+
+export default App;
